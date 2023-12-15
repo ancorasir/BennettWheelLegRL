041fe857c76b4606ecfe0a2c041d59c885b89fee
Ye-2077 <12111813@mail.sustech.edu.cn>
2023-12-15 22:52:06 +0800

    add bennett legged robots

---
+++ legged_gym/scripts/test.py
@@ -0,0 +1,156 @@
+"""
+Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+
+NVIDIA CORPORATION and its licensors retain all intellectual property
+and proprietary rights in and to this software, related documentation
+and any modifications thereto. Any use, reproduction, disclosure or
+distribution of this software and related documentation without an express
+license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+
+1080 balls of solitude
+-------------------------
+Demonstrates the use of collision filtering to limit collisions to actors within an environment,
+simulate all collisions including between actors in different environments, or simulate no collisions between
+actors - they will still collide with the ground plane.
+
+Modes can be set via command line arguments:
+    --no_collisions to have no actors colide with other actors
+    --all_collisions to have all actors, even those from different environments, collide
+
+Press 'R' to reset the  simulation
+"""
+
+import numpy as np
+from isaacgym import gymutil
+from isaacgym import gymapi
+from math import sqrt
+
+# initialize gym
+gym = gymapi.acquire_gym()
+
+# parse arguments
+args = gymutil.parse_arguments(
+    description="Collision Filtering: Demonstrates filtering of collisions within and between environments",
+    custom_parameters=[
+        {"name": "--num_envs", "type": int, "default": 36, "help": "Number of environments to create"},
+        {"name": "--all_collisions", "action": "store_true", "help": "Simulate all collisions"},
+        {"name": "--no_collisions", "action": "store_true", "help": "Ignore all collisions"}])
+
+# configure sim
+sim_params = gymapi.SimParams()
+if args.physics_engine == gymapi.SIM_FLEX:
+    sim_params.flex.shape_collision_margin = 0.25
+    sim_params.flex.num_outer_iterations = 4
+    sim_params.flex.num_inner_iterations = 10
+elif args.physics_engine == gymapi.SIM_PHYSX:
+    sim_params.substeps = 1
+    sim_params.physx.solver_type = 1
+    sim_params.physx.num_position_iterations = 4
+    sim_params.physx.num_velocity_iterations = 1
+    sim_params.physx.num_threads = args.num_threads
+    sim_params.physx.use_gpu = args.use_gpu
+
+sim_params.use_gpu_pipeline = False
+if args.use_gpu_pipeline:
+    print("WARNING: Forcing CPU pipeline.")
+
+sim_params.up_axis = gymapi.UP_AXIS_Z
+sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+sim = gym.create_sim(args.compute_device_id, args.graphics_device_id, args.physics_engine, sim_params)
+if sim is None:
+    print("*** Failed to create sim")
+    quit()
+
+# add ground plane
+plane_params = gymapi.PlaneParams()
+plane_params.normal = gymapi.Vec3(0, 0, 1) # z-up!
+plane_params.distance = 0.5
+plane_params.static_friction = 1
+gym.add_ground(sim, plane_params)
+
+# create viewer
+viewer = gym.create_viewer(sim, gymapi.CameraProperties())
+if viewer is None:
+    print("*** Failed to create viewer")
+    quit()
+
+
+
+
+asset_root = "resources/robots"
+asset_file = "Quadruped-IsaacSim/Quadruped-IsaacSim.urdf"
+# asset_file = "urdf/ball.urdf"
+# asset_file = "bennett/urdf/bennett.urdf"
+# asset_file = "a1/urdf/a1.urdf"
+
+assert_options = gymapi.AssetOptions()
+# assert_options.flip_visual_attachments = True  ### change the z axis of the visual attachment
+asset = gym.load_asset(sim, asset_root, asset_file, assert_options)
+
+# set up the env grid
+num_envs = args.num_envs
+num_per_row = int(sqrt(num_envs))
+env_spacing = 1.25
+env_lower = gymapi.Vec3(-env_spacing, 0.0, -env_spacing)
+env_upper = gymapi.Vec3(env_spacing, env_spacing, env_spacing)
+
+envs = []
+
+# subscribe to spacebar event for reset
+gym.subscribe_viewer_keyboard_event(viewer, gymapi.KEY_R, "reset")
+
+# set random seed
+np.random.seed(17)
+
+for i in range(1):
+    # create env
+    env = gym.create_env(sim, env_lower, env_upper, num_per_row)
+    envs.append(env)
+
+    # create ball pyramid
+    pose = gymapi.Transform()
+    pose.r = gymapi.Quat(0, 0, 0, 1)
+    pose.p = gymapi.Vec3(0, 1, 0)
+
+    collision_group = 0
+    collision_filter = 0
+
+    ahandle = gym.create_actor(env, asset, pose, None, collision_group, collision_filter)
+    props = gym.get_actor_dof_properties(env, ahandle)
+    props["driveMode"].fill(gymapi.DOF_MODE_VEL)
+    props["stiffness"].fill(0.0)
+    props["damping"].fill(0.0)
+    gym.set_actor_dof_properties(env, ahandle, props)
+
+gym.viewer_camera_look_at(viewer, None, gymapi.Vec3(20, 5, 20), gymapi.Vec3(0, 1, 0))
+
+# create a local copy of initial state, which we can send back for reset
+initial_state = np.copy(gym.get_sim_rigid_body_states(sim, gymapi.STATE_ALL))
+
+
+while not gym.query_viewer_has_closed(viewer):
+
+    # Get input actions from the viewer and handle them appropriately
+    for evt in gym.query_viewer_action_events(viewer):
+        if evt.action == "reset" and evt.value > 0:
+            gym.set_sim_rigid_body_states(sim, initial_state, gymapi.STATE_ALL)
+
+    # step the physics
+    gym.simulate(sim)
+    gym.fetch_results(sim, True)
+
+    # update the viewer
+    gym.step_graphics(sim)
+    gym.draw_viewer(viewer, sim, True)
+
+    # Wait for dt to elapse in real time.
+    # This synchronizes the physics simulation with the rendering rate.
+    gym.sync_frame_time(sim)
+
+gym.destroy_viewer(viewer)
+gym.destroy_sim(sim)
+
+
+
+
