eafb23dd38634f621bbcd6c4172f4aba60309a80
EleanorQ1201 <12110147@mail.sustech.edu.cn>
2024-01-24 14:50:52 +0800

    add RCOT calculation

--- experiment_data/calculate.py
+++ experiment_data/calculate.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class DataAnalyser:
     def __init__(self) -> None:
-        self.experiment_folder = "2023-12-28_01-38-55"
+        self.experiment_folder = "2023-12-28_00-02-06"
         self.experiment_data_dir = "experiment_data"
         self.experiment_dir = os.path.join(self.experiment_data_dir, self.experiment_folder)
         self.env_ids = list(range(50))
@@ -14,7 +14,7 @@ class DataAnalyser:
 
     def read_and_calculate_data(self, experiment_dir, env_ids, target_env):
 
-        # Calculate COT of the target robot
+        # Calculate MCOT of the target robot
         for env_id in env_ids:
             if target_env is not None and env_id != target_env:
                 continue
@@ -32,24 +32,43 @@ class DataAnalyser:
             if os.path.exists(filename_base_vel):
                 df_base_vel = pd.read_csv(filename_base_vel)
 
-            # Calculate COT
+            # MCOT = E/(mgd)
             distance = self.calculate_distance(df_base_vel)
             E_m = self.calculate_mechanical_energy_consumption(df_joint_torques, df_joint_vel)
             m = 3.578209936079509+0.7011939191100404+2.4365544485369104+0.2812248770831842+0.2812427717615869+0.7011939191100404+2.4365544485369104+0.2812248770831842+0.2812427717615869+0.7011939191100404+2.4365544485369104+0.28122487708301536+0.2812427717616486+0.7011939191100404+2.4365544485369104+0.28122487708301536+0.2812427717616486
             g = 9.81
-            COT = E_m/m/g/distance
+            MCOT = E_m/m/g/distance
 
+            # RCOT = P/(mgwzd)
+            L = 0.511/2
+            W = 0.125/2
+            D = math.sqrt(L**2 + W**2)
+            total_RCOT = 0
+            for index, row in df_base_vel.iterrows():
+                omega = np.abs(row['base_vel_yaw'])
+                power = 0
+                for column in df_joint_torques.columns:
+                    if column.startswith('Joint_'):
+                        torque = df_joint_torques.loc[index,column]
+                        vel = df_joint_vel.loc[index,column]
+                        power = power + abs(torque*vel)
+                total_RCOT = total_RCOT + power / (m*g*omega*D)
 
+            RCOT = total_RCOT/df_base_vel.shape[0]
 
             print("*"*20)
             print("Robot number: ", target_env)
             # print("distance: ", distance)
             # print("E_m: ", E_m)
             # print("mass: ", m)
-            print("COT: ", COT)
+            print("MCOT: ", MCOT)
+            print("RCOT: ", RCOT)
+
             print("*"*20)
 
-            return COT
+            return MCOT, RCOT
+        
+  
 
 
 
@@ -118,29 +137,45 @@ class DataAnalyser:
 
         return energy
     
-    # Calculate COT
-    def cal_CoT(self):
-        total_COT = 0
-        cot_values = []
+    # Calculate MCOT
+    def cal_COT(self):
+        MCOT_values = []
+        RCOT_values = []
 
 
-        # Calculate CoT for all robots and get the average value
+        # Calculate MCOT and RCOT for all robots and get the average value
         for target_env in self.env_ids:
-            cot_value = self.read_and_calculate_data(self.experiment_dir, self.env_ids, target_env)
-            cot_values.append(cot_value)
+            MCOT_value, RCOT_value = self.read_and_calculate_data(self.experiment_dir, self.env_ids, target_env)
+            MCOT_values.append(MCOT_value)
+            RCOT_values.append(RCOT_value)
 
-        cot_values = np.array(cot_values)
 
-        # Exclude the outliers of COT data
-        filtered_values = self.exclude_outliers(cot_values)
+        MCOT_values = np.array(MCOT_values)
+        RCOT_values = np.array(RCOT_values)
 
-        # Get the average value of COT
-        average_COT = np.mean(cot_values)
-        average_COT_process = np.mean(filtered_values)
 
-        print("average_COT (before excluding outliers): ", average_COT)
-        print("average_COT (after excluding outliers): ", average_COT_process)
-        return average_COT
+        # Exclude the outliers of MCOT data
+        MCOT_filtered_values = self.exclude_outliers(MCOT_values)        
+        
+        # Exclude the outliers of RCOT dataiers(RCOT_values)
+        RCOT_filtered_values = self.exclude_outliers(RCOT_values)
+
+        # Get the average value of MCOT
+        average_MCOT = np.mean(MCOT_values)
+        average_MCOT_process = np.mean(MCOT_filtered_values)
+
+        # Get the average value of RCOT
+        average_RCOT = np.mean(RCOT_values)
+        average_RCOT_process = np.mean(RCOT_filtered_values)
+
+
+        print("average_MCOT (before excluding outliers): ", average_MCOT)
+        print("average_MCOT (after excluding outliers): ", average_MCOT_process)
+
+        print("average_RCOT (before excluding outliers): ", average_RCOT)
+        print("average_RCOT (after excluding outliers): ", average_RCOT_process)
+
+        return average_MCOT, average_RCOT
 
     # Exclude the outliers of data
     def exclude_outliers(self, data, sigma_threshold=3):
@@ -151,5 +186,5 @@ class DataAnalyser:
 
 if __name__ == '__main__':
     d = DataAnalyser()
-    d.cal_CoT()
+    d.cal_COT()
     
